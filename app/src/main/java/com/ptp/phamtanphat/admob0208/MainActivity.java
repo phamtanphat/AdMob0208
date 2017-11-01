78c3338c51615110bef873bfbbfe023395441eac
thuviengithub0208 <khoaphp@yahoo.com>
2017-11-01 19:05:07 +0700

    Initial commit

---
+++ app/src/main/java/com/ptp/phamtanphat/admob0208/MainActivity.java
@@ -0,0 +1,34 @@
+package com.ptp.phamtanphat.admob0208;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.google.android.gms.ads.AdListener;
+import com.google.android.gms.ads.MobileAds;
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdView;
+
+
+
+public class MainActivity extends AppCompatActivity {
+    private AdView mAdView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+//        ID ứng dụng: ca-app-pub-9467965476301660~9691789556
+//        ID đơn vị quảng cáo: ca-app-pub-9467965476301660/9472297708
+        MobileAds.initialize(this, "ca-app-pub-3940256099942544~3347511713");
+        mAdView = (AdView) findViewById(R.id.adView);
+        AdRequest adRequest = new AdRequest.Builder().build();
+        mAdView.loadAd(adRequest);
+        mAdView.setAdListener(new AdListener() {
+            @Override
+            public void onAdClosed() {
+                super.onAdClosed();
+            }
+        });
+
+    }
+}
